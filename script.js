e846d4e46c5a7fa7cbdf70f4117ec1046d0f83db
Zack <bostianz@gmail.com>
2015-12-10 10:39:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //Use an array to store all the questions. Each question, along with its choices and correct answer, should be stored in an object. 
-var allQuestions = [{
+var questions = [{
     question: 'Who was the president that never told a lie?',
     choices: ['George Washington', 'Zachary Taylor', 'George Bush', 'Abraham Lincoln'],
-    correctAnswer: 4
+    correctAnswer: 3
   }, {
     question: 'On what piece of territory was the Battle of Bunker Hill fought?',
     choices: ['The Mississippi River', 'Breeds Hill', 'Fort William Henry', 'Fort Niagra'],
-    correctAnswer: 2
+    correctAnswer: 1
   }, {
     question: 'Who was the Assistant Secretary of State for Inter-American Affairs from 1985 to 1989 who allegedly solicited $10 million from the Sultan of Brunei to purchase arms for the Contras?',
     choices: ['Eliot Abrams', 'John Hancock', 'Barack Obama', 'Cooper Bradly'],
@@ -15,58 +15,80 @@ var allQuestions = [{
 
 ];
 
+//index of array
+var index = 0;
+var correctAnswer = 0;
+
 var grabQuestion = document.getElementById('question');
 var nextButton = document.getElementById('btnNext');
 var backButton = document.getElementById('btnBack');
-var correctAnswer = 0;
+var score = document.getElementById('score');
 
 
 //Add first question to the page
-var addQuizQuestion = function() {
-  var storeQuestion = allQuestions[0].question;
-  grabQuestion.innerHTML = storeQuestion; 
+var addQuizQuestion = function(question) {
+  grabQuestion.innerHTML = question;
 }
-addQuizQuestion();
-
-//When next button is clicked
-//the current question is replaced by the next
-nextButton.onclick = function() {
-  
-}
-
 
 //create a function to add answers from the array and to the DOM.
 //Add answers of first question to page
-var addQuizAnswers = function() {
-  //store choices of question one
-  var storeAnswers = allQuestions[0].choices;
+var addQuizAnswers = function(answers) {
+  //store reference of UL.
+  var grabUl = document.getElementById('answers');
+  grabUl.innerHTML = '';
   //loop through the choices
-  for (var i = 0; i < storeAnswers.length; i++ ) {
-    //store reference of UL.
-    var grabUl = document.getElementById('answers');
+  for (var i = 0; i < answers.length; i++) {
     //dynamically create radio button
-    var radio =  document.createElement('input');
+    var radio = document.createElement('input');
     radio.setAttribute('type', 'radio');
+    radio.setAttribute('name', 'button');
     //dynamically create a li element
     var createLi = document.createElement('li');
     //append radio button to li element
     createLi.appendChild(radio);
+
+    //create text node that has answers of question one
+    var answerText = document.createTextNode(answers[i]);
+    //append answers to li element
+    createLi.appendChild(answerText);
+
     //append li element to ul
     grabUl.appendChild(createLi);
 
- 
-  }  
+  }
+}
+addQuizQuestion(questions[index].question);
+addQuizAnswers(questions[index].choices);
+
+
+//next question and choices appear when next button is clicked
+nextButton.addEventListener('click', function() {
+  if (index < questions.length) {
+    index++;
+    addQuizQuestion(questions[index].question);
+    addQuizAnswers(questions[index].choices);
+  }
+}, false);
+
+//When user clicks radio button
+//Determine of answers is correct or not
+//correctAnswer ++ if correct
+var determineScore = function() {
+  var radioButtons = document.getElementsByTagName('input');
+  radioButtons.addEventListener('click', function, false);
+
 }
 
 
+  
 
-//add validation to make sure the user picks an answer
 
-//verify whether each users answer is right
-//if right the user gets +1 to their score
 
-//total score displayed on last page
 
+
+
+
+//When back button is clicked, return previous quesiton.
 
 
 
